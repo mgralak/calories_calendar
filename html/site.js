1f0c1f3adc422db2029c7f07fdebf0d93f639d08
mgralak <matheush23@wp.pl>
2017-05-10 19:30:31 +0200

    new files set

--- html/site.js
+++ html/site.js
@@ -1,45 +1,51 @@
-
-
-
-
-
 $(document).ready(function(){
   $("#w1").click(function() {
-  $("#product").replaceWith($("#product").text("Brokuł - 34 kcal"));
+  $("#product").replaceWith($("#product").text("Brokuł"));
+  $("#product").replaceWith($("#product").val(34));
 });
   $("#w2").click(function() {
-  $("#product").replaceWith($("#product").text("Marchew - 41 kcal"));
+  $("#product").replaceWith($("#product").text("Marchew"));
+  $("#product").replaceWith($("#product").val(41));
 });
   $("#o1").click(function() {
-  $("#product").replaceWith($("#product").text("Banan - 89 kcal"));
+  $("#product").replaceWith($("#product").text("Banan"));
+  $("#product").replaceWith($("#product").val(89));
 });
   $("#o2").click(function() {
-  $("#product").replaceWith($("#product").text("Marchew - 53 kcal"));
+  $("#product").replaceWith($("#product").text("Marchew"));
+  $("#product").replaceWith($("#product").val(53));
 });
   $("#m1").click(function() {
-  $("#product").replaceWith($("#product").text("Pierś z kurczaka - 99 kcal"));
+  $("#product").replaceWith($("#product").text("Pierś z kurczaka"));
+  $("#product").replaceWith($("#product").val(99));
 });
   $("#m2").click(function() {
-  $("#product").replaceWith($("#product").text("Wołowina - 232 kcal"));
+  $("#product").replaceWith($("#product").text("Wołowina"));
+  $("#product").replaceWith($("#product").val(232));
 });
   $("#n1").click(function() {
-  $("#product").replaceWith($("#product").text("Jajka gotowane na twardo - 155 kcal"));
+  $("#product").replaceWith($("#product").text("Jajka gotowane na twardo"));
+  $("#product").replaceWith($("#product").val(155));
 });
   $("#n2").click(function() {
-  $("#product").replaceWith($("#product").text("Twaróg - 90 kcal"));
+  $("#product").replaceWith($("#product").text("Twaróg"));
+  $("#product").replaceWith($("#product").val(90));
 });
   $("#z1").click(function() {
-  $("#product").replaceWith($("#product").text("Makaron - 144 kcal"));
+  $("#product").replaceWith($("#product").text("Makaron"));
+  $("#product").replaceWith($("#product").val(144));  
 });
   $("#z2").click(function() {
   $("#product").replaceWith($("#product").text("Płatki owsiane"));
   $("#product").replaceWith($("#product").val(389));
 });
   $("#p1").click(function() {
-  $("#product").replaceWith($("#product").text("Czekolada mleczna - 535 kcal"));
+  $("#product").replaceWith($("#product").text("Czekolada mleczna"));
+  $("#product").replaceWith($("#product").val(535));
 });
   $("#p2").click(function() {
-  $("#product").replaceWith($("#product").text("Pepsi - 37 kcal"));
+  $("#product").replaceWith($("#product").text("Pepsi"));
+  $("#product").replaceWith($("#product").val(37));
 });
   $("#ilość").on('keypress', function(e){
   return e.metaKey ||
@@ -48,28 +54,20 @@ $(document).ready(function(){
     /[0-9]/.test(String.fromCharCode(e.which));
 });
   
-var clicks = 0;
+//var clicks = 0;
+//$("#addProduct").click(function(){
+//    if(clicks == 0){
+//    	$("#productResult").text($("#product").text() + " - " + $("#product").val()* $("#ilość2").val()/100 + " kcal");
+//    }else{
+//        $("#productResult2").text($("#product").text() + " - " + $("#product").val()* $("#ilość2").val()/100 + " kcal");
+//    }
+//    ++clicks;
+//});
+
 $("#addProduct").click(function(){
-    if(clicks == 0){
-    	$("#productResult").text($("#product").text() + " - " + $("#product").val()* $("#ilość2").val()/100 + " kcal");
-    }else{
-        $("#productResult2").text($("#product").text() + " - " + $("#product").val()* $("#ilość2").val()/100 + " kcal");
-    }
-    ++clicks;
-});
+	$("#productResult").prepend("<br />" + $("#product").text() + " - " + $("#product").val()* $("#ilość2").val()/100 + " kcal ");
+	$("#sum").text("Łączna ilość kalorii: " + ($("#sum").val() + $("#product").val()* $("#ilość2").val()/100 + " kcal"));
 });
 
 
-/*
-var = caloriesTable = {
-  "vegetables" : {
-    "broccoli" : 200,
-    "lettuce" : 100,
-    "beet" : 50
-  },
-  "meat" : {
-    "pork" : 250,
-    "beef" : 200
-  }
-}
-*/
+});

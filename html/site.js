fbac85e38f282b51591d9dfb5486560aa0f52093
piotrmusielak <muielakpiotr89@gmail.com>
2017-05-24 21:28:29 +0200

    adding database of products + styling

--- html/site.js
+++ html/site.js
@@ -1,96 +1,47 @@
+
+
 $(document).ready(function(){
-  $("#w1").click(function() {
-  $("#product").replaceWith($("#product").text("Brokuł"));
-  $("#product").replaceWith($("#product").val(34));
-});
-  $("#w2").click(function() {
-  $("#product").replaceWith($("#product").text("Marchew"));
-  $("#product").replaceWith($("#product").val(41));
-});
-  $("#o1").click(function() {
-  $("#product").replaceWith($("#product").text("Banan"));
-  $("#product").replaceWith($("#product").val(89));
-});
-  $("#o2").click(function() {
-  $("#product").replaceWith($("#product").text("Marchew"));
-  $("#product").replaceWith($("#product").val(53));
-});
-  $("#m1").click(function() {
-  $("#product").replaceWith($("#product").text("Pierś z kurczaka"));
-  $("#product").replaceWith($("#product").val(99));
-});
-  $("#m2").click(function() {
-  $("#product").replaceWith($("#product").text("Wołowina"));
-  $("#product").replaceWith($("#product").val(232));
-});
-  $("#n1").click(function() {
-  $("#product").replaceWith($("#product").text("Jajka gotowane na twardo"));
-  $("#product").replaceWith($("#product").val(155));
-});
-  $("#n2").click(function() {
-  $("#product").replaceWith($("#product").text("Twaróg"));
-  $("#product").replaceWith($("#product").val(90));
-});
-  $("#z1").click(function() {
-  $("#product").replaceWith($("#product").text("Makaron"));
-  $("#product").replaceWith($("#product").val(144));  
-});
-  $("#z2").click(function() {
-  $("#product").replaceWith($("#product").text("Płatki owsiane"));
-  $("#product").replaceWith($("#product").val(389));
-});
-  $("#p1").click(function() {
-  $("#product").replaceWith($("#product").text("Czekolada mleczna"));
-  $("#product").replaceWith($("#product").val(535));
-});
-  $("#p2").click(function() {
-  $("#product").replaceWith($("#product").text("Pepsi"));
-  //$("#product").replaceWith($("#product").val(37));
-  caloriesSum = caloriesSum + 37;
-});
-  $("#ilość").on('keypress', function(e){
+
+
+  $(".selector").click(function() {
+    clickedValue = $(this).text();
+    productsCalories = productList[0][clickedValue];
+    console.log(clickedValue, productsCalories);
+    $("#product").html(`${clickedValue} ${productsCalories} kcal`);
+  });
+
+$("#ilość").on('keypress', function(e){
   return e.metaKey ||
     e.which <= 0 ||
     e.which == 8 ||
     /[0-9]/.test(String.fromCharCode(e.which));
+
 });
 
 var caloriesSum = 0;
-
-  
-//var clicks = 0;
-//$("#addProduct").click(function(){
-//    if(clicks == 0){
-//    	$("#productResult").text($("#product").text() + " - " + $("#product").val()* $("#ilość2").val()/100 + " kcal");
-//    }else{
-//        $("#productResult2").text($("#product").text() + " - " + $("#product").val()* $("#ilość2").val()/100 + " kcal");
-//    }
-//    ++clicks;
-//});
+var productsCalories;
+var clickedValue;
+var totalCaloriesSum = 0;
 
 $("#products").hide();
 
+function countCalories (_calories, _grams) {
+    var gramsToInt = parseInt(_grams);
+    var caloriesToInt = parseInt(_calories);
+    var result =  caloriesToInt * gramsToInt;
+    totalCaloriesSum = totalCaloriesSum + result;
+    return result;
+}
+
 $("#addProduct").click(function(){
   $("#products").show();
-
-  function countCalories (_calories, _grams) {
-    var result = _calories * _grams / 100 ;
-    caloriesSum = caloriesSum + result;
-    console.log(caloriesSum)
-    return result;
-  }
-
-  var caloriesPerItem = $("#product").val();
-  var itemAmount = $("#ilość2").val();
-  var prependedText = countCalories(caloriesPerItem, itemAmount) + ' kcal';
-  var resultSummary = 'Łączna ilość kalorii = ' + caloriesSum.toFixed(2);
-
-
-
-  $("#productResult").prepend("<br />" + "- " + $("#product").text() + " " + prependedText);
-	$("#sum").html(resultSummary);
-
+  var gramsAmount = $("#grams_amount").val();
+  $("#productResult").prepend(`<br /> ${clickedValue} ${countCalories(productsCalories, gramsAmount)} kcal`);
+  var resultSummary = 'Łączna ilość kalorii = ' + totalCaloriesSum;
+  $("#sum").html(resultSummary);
 });
+
+
 
 $("#meals").hide();
 
@@ -99,43 +50,27 @@ $("#addMeal").click(function(){
   $("#mealResult").prepend("<br />" + "Posiłek = " + $("#sum").text());
   $("#sum").text('Łączna ilość kalorii = ');
   $("#productResult").text("");
-  $("#sum").val(null);
+  caloriesSum = 0;
   $('#productResult').html(null);
 });
 
 
 });
-/*
-function Raccoon(name) {
-  this.intelligence = "very high";
-  this.name = name;
-  this.motto = function () {
-    console.log('screw dogs love raccoons');
-  }
-}
 
-var andrzej = new Raccoon('Andrzej');
- //klasa
-function Vegetables() {
-  this.products = {
-    'sałata' : 25,
-    'marchew' : 27,
-    'por' : 15
-  }
-}
-// json
-products = {
-  vegetables {
-    'marchew' : 25,
-    'kapusta' : 27
-  },
-
-  meat {
-    'słonina' : 5000,
-    'boczek' : 25000
-  }
-
-}
-*/
+ 
+var productList = [{
+    'Marchew' : 25,
+    'Brokuł' : 27,
+    'Banan' : 45,
+    'Pomarańcza' : 34,
+    'Jaja' : 54,
+    'Twaróg' : 34,
+    'Pierś z kurczaka' : 200,
+    'Wołowina' : 300,
+    'Makaron' : 70,
+    'Płatki owsiane' : 45,
+    'Czekolada mleczna' : 500,
+    'Pepsi' : 10000
+}];
 
 
